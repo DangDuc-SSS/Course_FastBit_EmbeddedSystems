0e5aee80a51e13249fd77334a9aee6cfe0d8267b
abdosalem490 <abdelrahman.ibrahim01@eng-st.cu.edu.eg>
2022-06-12 23:25:47 +0200

    ..

---
+++ 01 Microcontroller Embedded C Programming Absolute Beginners/course codes written by me/19 Bitwise shift operators/exercises/03 printing even numbers between 2 nums using for loop/main_new.c
@@ -0,0 +1,53 @@
+/*
+ * main_new.c
+ *
+ *  Created on: Jun 4, 2022
+ *      Author: abdo
+ */
+
+/*
+ * main.c
+ *
+ *  Created on: Jun 4, 2022
+ *      Author: abdo
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+void wait_for_user_input(void);
+
+int main(void) {
+	int32_t num1 = 0;
+	int32_t num2 = 0;
+	uint32_t numOfEvenNum = 0;
+	printf("Enter the start and end numbers (give space between 2 nos) :");
+	scanf("%d%d", &num1, &num2);
+	printf("%d\t%d\n",num1,num2);
+	printf("Even numbers are\n");
+
+	if(num2 < num1){
+		printf("ending number should be > starting number\n");
+		wait_for_user_input();
+	}
+
+	for(;num1<=num2;num1++){
+		if(!(num1 & 1)){
+			printf("%d\t",num1);
+			numOfEvenNum++;
+			fflush(stdout);
+		}
+	}
+	printf("\nTotal number of even nos = %u\n",numOfEvenNum);
+
+	wait_for_user_input();
+	return 0;
+}
+
+void wait_for_user_input(void) {
+	// wait till the user press enter
+	printf("click enter to exit\n");
+	while (getchar() != '\n')
+		;
+	getchar();
+}
+
