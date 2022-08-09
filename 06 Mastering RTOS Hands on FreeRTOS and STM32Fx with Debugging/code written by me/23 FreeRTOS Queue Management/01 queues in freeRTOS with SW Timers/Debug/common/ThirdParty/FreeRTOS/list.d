0880b524183b5481887fa73f277a791a26d8f4aa
abdosalem490 <abdelrahman.ibrahim01@eng-st.cu.edu.eg>
2022-08-09 03:41:57 +0200

    9/8/2022

---
+++ 06 Mastering RTOS Hands on FreeRTOS and STM32Fx with Debugging/code written by me/23 FreeRTOS Queue Management/01 queues in freeRTOS with SW Timers/Debug/common/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,26 @@
+common/ThirdParty/FreeRTOS/list.o: \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/list.c \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/list.h
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+../Core/Inc/FreeRTOSConfig.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/projdefs.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/portable.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/list.h:
---
+++ 06 Mastering RTOS Hands on FreeRTOS and STM32Fx with Debugging/code written by me/23 FreeRTOS Queue Management/01 queues in freeRTOS with SW Timers/Debug/common/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,29 @@
+common/ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/list.h
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+../Core/Inc/FreeRTOSConfig.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/projdefs.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/portable.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/task.h:
+
+C:/Users/abdo/Desktop/stm32CubeIDE\ workspace/common/ThirdParty/FreeRTOS/include/list.h:
